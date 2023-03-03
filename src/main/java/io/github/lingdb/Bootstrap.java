02aeb43c23a7a9f04c604773bb85a08234eb376f
alin <rongguilin@iyunmai.com>
2023-03-03 16:16:39 +0800

    log config

--- src/main/java/io/github/lingdb/Bootstrap.java
+++ src/main/java/io/github/lingdb/Bootstrap.java
@@ -1,7 +1,13 @@
 package io.github.lingdb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Bootstrap {
+    private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
+    
     public static void main(String[] args) {
-        System.out.println("logging ,.......");
+        logger.info("bootstrap ling db.");
+        logger.info("ling db started.");
     }
 }
