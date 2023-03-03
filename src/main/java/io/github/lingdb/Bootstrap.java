31c68d7a6cd4638e68fd141ee3415715d821c2f5
alin <rongguilin@iyunmai.com>
2023-03-03 14:43:55 +0800

    buil jar plugin

--- src/main/java/io/github/lingdb/Bootstrap.java
+++ src/main/java/io/github/lingdb/Bootstrap.java
@@ -1,7 +1,7 @@
 package io.github.lingdb;
 
-public class Boot {
+public class Bootstrap {
     public static void main(String[] args) {
-        
+        System.out.println("logging ,.......");
     }
 }
